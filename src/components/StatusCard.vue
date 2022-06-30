7ff01f2d8a9afb2a1177a7ac773e87db9a67dd52
Catalin Bratu <bratu.acb@gmail.com>
2022-06-30 19:57:18 +0300

    add mock dashboard status cards

--- src/components/StatusCard.vue
+++ src/components/StatusCard.vue
@@ -2,15 +2,20 @@
   <BaseCard>
     <div class="grid grid-cols-3 gap-4">
       <div class="col-span-1 m-auto">
-        <img class="h-16 w-16" src="@/assets/icons/stats.svg" />
+        <slot name="icon" />
       </div>
       <div class="col-span-2 font-bold text-black">
-        <div class="text-xl">Active Devices</div>
-        <div class="text-4xl">32</div>
+        <div class="text-lg">{{ props.header }}</div>
+        <div class="text-3xl">{{ props.status }}</div>
       </div>
     </div>
   </BaseCard>
 </template>
 <script setup lang="ts">
 import BaseCard from "@/components/BaseCard.vue";
+
+const props = defineProps<{
+  header: string;
+  status: string;
+}>();
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="p-4">
     <div class="container mx-auto">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
         <div class="md:col-span-1">
           <div class="flex flex-col gap-8">
-            <StatusCard class="bg-cyan" />
-            <StatusCard class="bg-cyan" />
-            <StatusCard class="bg-cyan" />
+            <StatusCard class="bg-cyan" header="Active Devices" status="32">
+              <template #icon>
+                <img class="h-16 w-16" src="@/assets/icons/stats.svg" />
+              </template>
+            </StatusCard>
+            <StatusCard
+              class="bg-green-cyan"
+              header="System Health"
+              status="Ok"
+            >
+              <template #icon>
+                <img class="h-16 w-16" src="@/assets/icons/health.svg" />
+              </template>
+            </StatusCard>
+            <StatusCard
+              class="bg-yellow"
+              header="Energy Levels"
+              status="204 Watts"
+            >
+              <template #icon>
+                <img class="h-16 w-16" src="@/assets/icons/lightning.svg" />
+              </template>
+            </StatusCard>
           </div>
         </div>
-        <div class="md:col-span-2">2/3</div>
+        <div class="md:col-span-3">3/4</div>
       </div>
     </div>
   </div>
